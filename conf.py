4d3c5b02086cb6992450b7e95e79e0398888ca4b
startwithquicken <merryflee66+9@gmail.com>
2026-02-12 15:42:36 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,8 +1,11 @@
-project = 'Quicken Help Guide'
-author = 'Independent Documentation'
+project = 'Quicken Desktop Guide'
+author = 'Quicken Desktop Team'
 release = '1.0'
 
 extensions = []
+
+# Templates directory (safe to keep empty)
+templates_path = ['_templates']
 
 exclude_patterns = [
     '_build',
@@ -13,17 +16,12 @@ exclude_patterns = [
     '.DS_Store',
 ]
 
-html_theme = 'alabaster'
+# Use Read the Docs theme
+html_theme = 'sphinx_rtd_theme'
 
 html_static_path = ['_static']
 
-master_doc = 'index'
+html_title = "Quicken Desktop Download | Install, Activate & Update"
 
 html_show_sphinx = False
 html_show_copyright = True
-
-html_theme_options = {
-    'description': 'Independent step-by-step Quicken help documentation',
-    'show_powered_by': False,
-    'show_related': True,
-}
